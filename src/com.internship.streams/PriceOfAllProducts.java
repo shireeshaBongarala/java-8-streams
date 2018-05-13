d042e2b40c61f03618fb3c1a5a33fa657d74dc05
Shireesha Bongarala <shiresha@thoughtworks.com>
2018-05-13 19:52:47 +0530

    Add total price of all products.

---
+++ src/com.internship.streams/PriceOfAllProducts.java
@@ -0,0 +1,18 @@
+package com.internship.streams;
+
+import com.internship.streams.models.ProductListing;
+
+public class PriceOfAllProducts {
+    /* Given the products and the prices, our task is to find sum of all the prices on the product listing page*/
+    public static void main(String[] args) {
+        ProductListing productListing = DefaultProductListing.getDefaultProductListing();
+
+        double totalPriceOfAllTheProducts = getTotalPriceOfAllTheProducts(productListing);
+
+        System.out.println(totalPriceOfAllTheProducts);
+    }
+
+    private static double getTotalPriceOfAllTheProducts(ProductListing productListing) {
+        return 0.0;
+    }
+}
